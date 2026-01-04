48be643be8b3a9301007b21ca2a2b0fbba36c5c8
GitHubNetizenS <sta126get@126.com>
2026-01-04 15:48:11 +0800

    SpringBoot后端代码。

---
+++ src/main/java/com/kitchenmanagerspringboot/KitchenManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kitchenmanagerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenManagerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kitchenmanagerspringboot/KitchenManagerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchenmanagerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenManagerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
