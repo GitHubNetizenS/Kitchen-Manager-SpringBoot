69149166099cf0e13330d8997ade6164d05040e8
Tang Shunyi <1805658605@qq.com>
2026-01-11 17:09:37 +0800

    菜谱视频建表语句以及数据插入，爬虫系统

--- src/main/resources/sql/create_ingredient_idf_sql.sql
+++
@@ -1,7 +0,0 @@
-USE `kitchen-manager`;
-
-CREATE TABLE ingredient_idf (
-    ingredient_id   INT NOT NULL COMMENT    '原料ID，对应ingredient表主键',
-    idf_value       DOUBLE NOT NULL COMMENT '该原料的IDF值',
-    PRIMARY KEY (ingredient_id)
-) COMMENT='原料IDF全局表（TF-IDF用）';
